50fc71d2d6b11f0efbd9e8b5bb84fd94335aa0e0
andresogc <andresogc2011@gmail.com>
2021-04-11 15:06:20 -0500

    Primer commit

---
+++ src/test/java/com/andresogc/noticias/NoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresogc.noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
